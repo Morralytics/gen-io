9daea86c3ddd8a42fc5005060a06d99bed9a4165
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-23 11:25:20 -0700

    Utilizing tailwind for styling and styles from the global css file.

--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section>
-        <h1>
+    <section className='w-full flex-center flex-col'>
+        <h1 className='head_text text-center'>
             Generate & Share
             <br />
-            <span>AI-Powered Prompts</span>
+            <span className='orange_gradient text-center'> AI-Powered Prompts</span>
         </h1>
-        <p>
+        <p className='desc text-center'>
             Gen-io is an open-source AI prompting tool built for the modern world to generate, discover, and share innovative prompts
         </p>
     </section>
