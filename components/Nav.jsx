c6af286ee66e35f4440a947784486a43514b8f57
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-23 12:15:01 -0700

    Nav bar utilizing nexjs Link to bring back to homepage with the logo being the clickable link. Using tailwind, styles are being applied that will hid text for smaller devices and apply flexbox.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 // Import allows us to move through other pages of the app
 import Link from 'next/link';
 // Import will automatically optomize the image
@@ -10,7 +10,18 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image 
+          src='/assets/images/logo.svg'
+          alt='Gen-io Logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+        <p className='logo_text'>Gen-io</p>
+      </Link>
+    </nav>
   )
 }
 
