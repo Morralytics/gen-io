e978603b3bb488b5ac19bcfe90d88df3abc7d3ba
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-23 12:25:01 -0700

    Nav bar now has a create post link, sign out button, and an image placeholder that will later be used as a profile picture. With a combination of global.css and direct tailwind styling, this is styling the nav bar items.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,28 +1,57 @@
-'use client';
+"use client";
 // Import allows us to move through other pages of the app
-import Link from 'next/link';
+import Link from "next/link";
 // Import will automatically optomize the image
-import Image from 'next/image';
+import Image from "next/image";
 // Import hooks that will be used within this component
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 // Import will utilize nextjs to make signing in and out easy
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href='/' className='flex gap-2 flex-center'>
-        <Image 
-          src='/assets/images/logo.svg'
-          alt='Gen-io Logo'
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Gen-io Logo"
           width={30}
           height={30}
-          className='object-contain'
+          className="object-contain"
         />
-        <p className='logo_text'>Gen-io</p>
+        <p className="logo_text">Gen-io</p>
       </Link>
-    </nav>
-  )
-}
 
-export default Nav
+      {/* Desktop Nav */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src=""
+                alt="profile"
+                width={37}
+                height={37}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
