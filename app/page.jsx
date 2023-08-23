c13deabaf5b649464b2f777587f244e51815f306
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-23 11:11:52 -0700

    Basic home page structural layout.

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section>
+        <h1>
+            Generate & Share
+            <br />
+            <span>AI-Powered Prompts</span>
+        </h1>
+        <p>
+            Gen-io is an open-source AI prompting tool built for the modern world to generate, discover, and share innovative prompts
+        </p>
+    </section>
   )
 }
 
