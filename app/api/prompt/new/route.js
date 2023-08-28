5cc452617059e409d914030cceb087a57a5c2148
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-28 14:12:40 -0700

    Finished the prompt submission api.

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,11 +1,21 @@
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/Prompt";
 
-export const POST = async (req, res) => {
-    const { userId, pormpt, tag } = await req.json();
+export const POST = async (req) => {
+    const { userId, prompt, tag } = await req.json();
 
     try {
-        await connectToDB()
+        await connectToDB();
+        const newPrompt = new Prompt({
+            creator: userId,
+            prompt,
+            tag,
+        });
+
+        await newPrompt.save();
+
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
     } catch (err) {
-        
+        return new Response('Failed to create a new prompt', { status: 500 });
     }
 }
