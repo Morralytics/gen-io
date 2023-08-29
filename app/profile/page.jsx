e0e6bad1c46a6f7ac9506273f88ab07dccac3e5f
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-29 14:32:36 -0700

    Working on the edit prompt page using code from the create prompt form as a template.

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,44 +1,44 @@
-'use client'
+"use client";
 
-import { useState, useEffect} from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-import Profile from '@components/Profile';
+import Profile from "@components/Profile";
 
 const MyProfile = () => {
-    const { data: session } = useSession();
-    const [posts, setPosts] = useState([])
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const response = await fetch(`/api/users/${session?.user.id}/posts`);
-            const data = await response.json();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
 
-            setPosts(data);
-        }
+      setPosts(data);
+    };
 
-        if(session?.user.id) fetchPosts();
-    }, [])
+    if (session?.user.id) fetchPosts();
+  }, []);
 
-    const handleEdit = () => {
-      
-    }
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-    const handleDelete = () => {
-
-    }
-
+  const handleDelete = (post) => {
+    
+  };
 
   return (
-    <Profile 
-        name='My'
-        desc='Welcome to your personalized profile page'
-        data={posts}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
     />
-  )
-}
+  );
+};
 
 export default MyProfile;
