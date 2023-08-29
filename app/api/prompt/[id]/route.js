bee46df90c764381c42e0806e43294827dbfc6f7
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-29 13:56:44 -0700

    Finished the update route.

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -16,5 +16,22 @@ export const GET = async (req, { params }) => {
     }
 }
 // PATCH (update)
+export const PATCH = async(req, { params }) => {
+    const { prompt, tag } = await req.json();
 
+    try {
+        await connectToDB();
+
+        const existingPrompt = await Prompt.findById(params.id);
+
+        if(!prompt) return new Response('Prompt not found!', { status: 404 });
+        
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        return new Response(JSON.stringify(existingPrompt), { status: 200 });
+    } catch (error) {
+        return new Response('Failed to update prompt', { status: 500 });
+    }
+}
 // DELETE (delete)
